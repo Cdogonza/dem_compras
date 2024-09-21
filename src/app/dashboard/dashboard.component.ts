c15076e406df376bbbe938cae25b467b3d5f5fd9
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-09-21 01:08:29 -0300

    nav bar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { DataService } from '../data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  tableData: any[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.tableData = data;
+    });
+  }
 }
+
+
+
+
+
